6e5fd7c7246184b10c76eb282786e98548502d24
sarbendrasigdel <sarmendrasigdel5019@gmail.com>
2024-05-07 13:12:41 +0545

    changes made to handle files

--- uploader.php
+++ uploader.php
@@ -9,39 +9,34 @@ if(isset($_FILES['file']))
 
 $file_arr = $_FILES['file'];
     
-$ftp_server = "serverName";
-$ftp_user = "username";
-$ftp_password = "password";
+$ftp_server = "192.168.1.143";
+$ftp_user = "user";
+$ftp_password = "admin";
 
 $ftp_conn = ftp_connect($ftp_server) or die("could not connect to the server");
 
 if($ftp_conn)
 {
-    echo "connection successfull!! \n";
+    echo "connection successfull!!". "<br/>" ;
 }
 
 $login = ftp_login($ftp_conn, $ftp_user, $ftp_password);
 
-// $local_file = $file_tmp_name;
-// // $fp = fopen($local_file,"r");
-// $server_file = $file_name;
-
-foreach($file_arr['name']as $key => $value){
  
 
-    $local_file = $file_arr['tmp_name'][ $key ];
-    $server_file = $value;
+    $local_file = $file_arr['tmp_name'];
+    $server_file = $file_arr['name'];
    
      if(ftp_put($ftp_conn,$server_file ,$local_file,FTP_BINARY))
      {
-         echo  $server_file." uploaded successfully!";
+         echo  "File uploaded successfully!"."<br/>" ;
         }
         
         else{
             
-            echo "file upload failed!";
+            echo "file upload failed!" ." <br/>" ;
         }
-    }
+    
     
 
 ftp_close($ftp_conn);
